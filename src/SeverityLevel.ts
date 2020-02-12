c88099fccc000574cd8291330bdea62686436c93
ldziewa <ldziewa@trans.eu>
2020-02-12 11:12:10 +0100

    feat: Add support severity level

---
+++ src/SeverityLevel.ts
@@ -0,0 +1,14 @@
+export enum SeverityLevel {
+    EMERG = 0,
+    EMERGENCY = 0,
+    ALERT = 1,
+    CRIT = 2,
+    CRITICAL = 2,
+    ERR = 3,
+    ERROR = 3,
+    WARN = 4,
+    WARNING = 4,
+    NOTICE = 5,
+    INFO = 6,
+    DEBUG = 7,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
 import { IncomingWebhook, IncomingWebhookDefaultArguments } from "@slack/webhook";
+import { SeverityLevel } from "./SeverityLevel";
 
 let webhook: IncomingWebhook;
+let severityLevel: number = SeverityLevel.DEBUG;
 
 export interface IPluginConfig extends IncomingWebhookDefaultArguments {
     url: string;
+
+    level?: string;
+
     [key: string]: any;
 }
 
 export interface ILayouts {
     basicLayout: string,
+
     [key: string]: any;
+
     layout(name: string, config: object): any;
+}
+
+function logLevelIsGreaterThanOrEqualTo(level: string): boolean {
+    return severityLevel >= SeverityLevel[level as keyof typeof SeverityLevel];
 }
 
 function appender(layout: any) {
     return (loggingEvent: any) => {
+        if (!logLevelIsGreaterThanOrEqualTo(loggingEvent.level.levelStr.toUpperCase())) {
+            return;
+        }
+
         webhook
             .send(layout(loggingEvent))
             .catch(err => console.error('log4js:slack - Error sending log to slack: ', err));
@@ -25,6 +40,13 @@ export function configure(config: IPluginConfig, layouts: ILayouts): Function {
     let layout = layouts.basicLayout;
     if (config.layout) {
         layout = layouts.layout(config.layout.type, config.layout);
+    }
+
+    if (config.level) {
+        severityLevel = SeverityLevel[config.level.toUpperCase() as keyof typeof SeverityLevel];
+        if (typeof severityLevel === 'undefined') {
+            throw new Error('Unsupported severity level');
+        }
     }
 
     webhook = new IncomingWebhook(config.url, {
